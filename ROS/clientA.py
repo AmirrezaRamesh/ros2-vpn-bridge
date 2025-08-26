5614338b095070b7e7fb078f0119ebad701764f5
AmirrezaRamesh <rameshamirreza3@gmail.com>
2025-08-26 18:04:48 +0330

    comments added

--- ROS/clientA.py
+++ ROS/clientA.py
@@ -2,30 +2,42 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
-#send the data to clientB
+#we send the data over VPN
 
+#ROS2 Node
 class Send_data(Node):
+    
     def __init__(self):
+
         super().__init__('publisher')
+
         #publisher
         self.publisher = self.create_publisher(
             String,
             "Message",
             10)
+        
         #timer
         self.timer = self.create_timer(1, self.timer_callback)
 
+    #timer callback
     def timer_callback(self):
         msg = String()
         msg.data = 'VPN worked!'
+
         self.publisher.publish(msg)
+
         self.get_logger().info('Sending my regards to Machine B : "%s"' %msg.data)
-    
+
+#main function
 def main(args=None):
+
     rclpy.init(args=args)
+
     publisher = Send_data()
     rclpy.spin(publisher)
     publisher.destroy_node()
+
     rclpy.shutdown()
 
 if __name__ == '__main__':
--- ROS/clientB.py
+++ ROS/clientB.py
@@ -2,25 +2,35 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
-#receive the data
+#we receive the data over VPN
 
+#ROS2 Node
 class Receive_Data(Node):
+
     def __init__(self):
+        
         super().__init__('subscriber')
+
         #subscriber
         self.subscriber = self.create_subsribtion(
         String,
         "Message",
         self.listener_callback,
         10)
+
+    #listener callback
     def listener_callback(self, msg):
+
         self.get_logger().info('Receiving Machine A regards: %s' %msg.data)
-    
+
+#main function
 def main(args=None):
     rclpy.init(args=args)
+
     subscriber = Receive_Data()
     rclpy.spin(subscriber)
     subscriber.destroy_node()
+
     rclpy.shutdown()
 
 if __name__ == '__main__':
